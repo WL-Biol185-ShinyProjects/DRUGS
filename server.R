5aeed2856169d0bf6f52f60ed9c04095e509112c
Albert Zhu <zhua25@mail.wlu.edu>
2024-11-04 15:23:39 -0500

    UI and server with Fixed neat code

--- server.R
+++ server.R
@@ -1,32 +1,15 @@
-library(leaflet)
-library(tidyverse)
 library(shiny)
-library(ggplot2)
 library(dplyr)
 
-source("Home_page.R")
+source("home.R")
+source("medicalUses.R")
+source("tab3.R")
+source("tab4.R")
+source("tab5.R")
+source("tab6.R")
+source("tab7.R")
 
-
-function(input, output) {
-  observeEvent(input$learnMore, {
-    showModal(modalDialog(
-      title = "Symptoms",
-      "Here you'll find detailed information on various types of drugs."
-    ))
-  })
+function(input, output, session) {
   
-  observeEvent(input$research, {
-    showModal(modalDialog(
-      title = "Alternates",
-      "This section is dedicated to cutting-edge research on drug safety and efficacy."
-    ))
-  })
   
-  observeEvent(input$contact, {
-    showModal(modalDialog(
-      title = "Contact Us",
-      "Feel free to contact us at info@druginfohub.com."
-    ))
-  })
 }
-
