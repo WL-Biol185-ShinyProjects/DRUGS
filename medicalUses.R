2bc1d23d493891bc9394e8ccfd52bbb0e146d883
Albert Zhu <zhua25@mail.wlu.edu>
2024-11-04 15:29:20 -0500

    medical Uses tab code

---
+++ medicalUses.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(dplyr)
+medicalUses <- tabPanel(
+  "Name1",
+  tags$h2("What medical uses your medicine has"), 
+  mainPanel(
+    
+  ) 
+)
+  
+function(input, output) {
+  observeEvent(input$learnMore, {
+    showModal(modalDialog(
+      title = "Symptoms",
+      "Here you'll find detailed information on various types of drugs."
+    ))
+  })
+  
+  observeEvent(input$research, {
+    showModal(modalDialog(
+      title = "Alternates",
+      "This section is dedicated to cutting-edge research on drug safety and efficacy."
+    ))
+  })
+  
+  observeEvent(input$contact, {
+    showModal(modalDialog(
+      title = "Contact Us",
+      "Feel free to contact us at info@druginfohub.com."
+    ))
+  })
+}
+
