21bd371e1c9b55c8765c74e8d9e8f520e328bfb4
Tade Omisore <omisoret25@mail.wlu.edu>
2024-12-02 16:35:36 -0500

    To make work

--- Clinical_Trails.R
+++ Clinical_Trails.R
@@ -1,14 +1,20 @@
+
+
+Clinical_Traillls1 <- read.csv("ctg_studie_(4).csv")
+
+
+
 Clinical_Trails <-
   
-<<<<<<< HEAD
+
   tabPanel("Clinical Trial Matching",
 
   
                         sidebarLayout(
                           sidebarPanel(
-                            selectInput("Locations", "Select Location:", choices = NULL), # Populated dynamically
-                            selectInput("Conditions", "Select Condition:", choices = NULL), # Populated dynamically
-                            selectInput("StudyStatus", "Select Study Status:", choices = NULL), # Populated dynamically
+                            selectInput("Locations", "Select Location:", choices = ctg_studie_(4)), # Populated dynamically
+                            selectInput("Conditions", "Select Condition:", choices = ctg_studie_(4)), # Populated dynamically
+                            selectInput("StudyStatus", "Select Phase:", choices = ctg_studie_(4)), # Populated dynamically
                             actionButton("search", "Search")
                           ),
                           mainPanel(
@@ -18,30 +24,29 @@ Clinical_Trails <-
                )
              
            
-           
-=======
-  tabPanel("Clinical Trial Matching")
+
+#  tabPanel("Clinical Trial Matching")
 
 
 
 
 
 
-sidebarLayout(
-  sidebarPanel(
-    textInput("name", "Patient Name:", value = ""),
-    selectInput("condition", "Current Health Condition:",
-                choices = c("Select Condition", "Cancer", "Diabetes", "Cardiovascular", "Asthma", "Arthritis")),
-    selectInput("history", "Treatment History:",
-                choices = c("Select Treatment", "Chemotherapy", "Insulin", "Immunotherapy", "Statins", "Oral medication")),
-    textInput("location", "Location (City/State):", value = ""),
-    actionButton("search", "Find Matching Trials")
-  ),
+#sidebarLayout(
+ # sidebarPanel(
+  #  textInput("name", "Patient Name:", value = ""),
+   # selectInput("condition", "Current Health Condition:",
+    #            choices = c("Select Condition", "Cancer", "Diabetes", "Cardiovascular", "Asthma", "Arthritis")),
+#    selectInput("history", "Treatment History:",
+ #               choices = c("Select Treatment", "Chemotherapy", "Insulin", "Immunotherapy", "Statins", "Oral medication")),
+ #   textInput("location", "Location (City/State):", value = ""),
+#    actionButton("search", "Find Matching Trials")
+ # ),
   
-  mainPanel(
-    tableOutput("results")
-  )
-)
+  #mainPanel(
+   # tableOutput("results")
+  #)
+#)
 
 
->>>>>>> e23ac17c742d02229554e902c66ba7f9166178ce
+
