299df4e2d22a84c72c975f0c864e7ca287007e2b
Tade Omisore <omisoret25@mail.wlu.edu>
2024-12-04 15:36:11 -0500

    fix

--- Clinical_Trails.R
+++ Clinical_Trails.R
@@ -3,10 +3,9 @@ library(dplyr)
 library(DT)
 
 # Load dataset
-<<<<<<< HEAD
-=======
+
 clinical_trials <- read.csv("ClinicalST.csv")
->>>>>>> f6e85dbb72df276ef798b0168acc364815e2cbc6
+
 
 clinical_trials <- read.csv("ClinicalST.csv")
 
