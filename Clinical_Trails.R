00af9371359a7ddab81e4a5fd71cf93343b7fc52
Danielpmr778 <chandlerd25@mail.wlu.edu>
2024-12-10 10:44:47 -0500

    fix merge

--- Clinical_Trails.R
+++ Clinical_Trails.R
@@ -4,39 +4,33 @@ library(DT)
 
 # Load dataset
 
-clinical_trials <- read.csv("ClinicalST.csv")
-
-
 
 clinical_trials <- read.csv("ClinicalST.csv")
 
-
-Clinical_Trials <-   tabPanel ("Clinical Trial",
-  #sidebarLayout(
+Clinical_Trials <-   tabPanel ("Clinical Trial")
+  sidebarLayout(
     sidebarPanel(
       selectizeInput("condition", "Select Condition(s):", 
-                     choices = NULL, 
+                     choices = unique(clinical_trials$Conditions), 
                      options = list(maxOptions = 10),
                      multiple = TRUE),
       
       
       selectizeInput("gender", "Select Gender(s):", 
-                     choices = NULL, 
+                     choices = unique(clinical_trials$Sex), 
                      multiple = TRUE),
       
       
       
       selectizeInput("phase", "Select Phase(s):", 
-                     choices = NULL, 
+                     choices = unique(na.omit(clinical_trials$Phases)), 
                      multiple = TRUE),
       actionButton("search", "Search"),
       actionButton("reset", "Reset Filters")
     ),
-    
     mainPanel(
       textOutput("summary"),
       DT::dataTableOutput("results")
     )
-)
-  #)
+  )
 
